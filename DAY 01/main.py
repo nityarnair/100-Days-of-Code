20a884979972d1a4a6effd6c90a23e775e92baf0
NITYA R NAIR <nityarnair07@gmail.com>
2026-06-25 15:12:12 +0530

    Create main.py

---
+++ DAY 01/main.py
@@ -0,0 +1,4 @@
+print("WELCOME TO BAND NAME GENERATOR!")
+city=input("Which is the city that you grew up in?\n")
+pet=input("What is your pet's name?\n")
+print("Your band's name can be " + city + pet)
